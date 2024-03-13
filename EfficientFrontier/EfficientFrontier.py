6cead1f2fa5ea1cbed95f0ce2317c4f2b1745420
MikeGibb7 <119349139+MikeGibb7@users.noreply.github.com>
2024-03-13 16:02:42 -0400

    change header for fetching

--- EfficientFrontier/EfficientFrontier.py
+++ EfficientFrontier/EfficientFrontier.py
@@ -249,6 +249,7 @@ Desc: Main method, creates stock list, start and end dates, gets stock data, gen
 '''
 def main():
 
+    urlib3.disable_warnings()
     symbols = ["AAPL", "META", "MSFT", "GOOGL", "AMD", "KO", "J", "ALLY", "BMO"] # Add tickers you would like to be in the portfolios
     # Date range
     start = '2023-01-01'
